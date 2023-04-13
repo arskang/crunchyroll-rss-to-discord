f46e02b84627ffabac9f33fef693de3eaf1cdad8
Edder Silva <arskang@gmail.com>
2023-04-13 00:23:46 -0600

    Optimización

--- api/services/index.ts
+++ api/services/index.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import * as moment from 'moment';
 import { parse } from 'rss-to-json';
-// import { Between } from "typeorm"
+import { In } from "typeorm"
 
 import db from '../db';
 import { CrunchyrollRSS, Item } from '../models/crunchyroll';
-import { Discord } from '../models/discord';
+import { Discord, Embed } from '../models/discord';
 import { Logs } from '../models/postgress';
 
 import 'moment/locale/es-mx';
@@ -13,35 +13,9 @@ moment.locale('es-mx');
 
 export const sleep = () => new Promise<boolean>(res => setTimeout(() => res(true), 30000));
 
-async function* sendWebhook(items: Item[]) {
-  if (!db.isInitialized) {
-    throw new Error('DB has not initialized');
-  }
-
-  for(let i = 0; i < items.length; i++) {
-    const discordMessage: Discord = {
-      embeds: [
-        {
-          title: items[i].title,
-          url: items[i].link,
-          color: 16020769, // #f47521
-          description: moment.unix(items[i].published/1000).format('LLLL'),
-          image: { url: items[i].description.replace(/(.+src=\")(.+)(\".+)/, "$2") } ,
-        },
-      ],
-    }
-
-    // if (i > 0) yield sleep();
-    yield axios.post(process.env.DISCORD_WEBHOOK || '', discordMessage);
-  
-    yield db.createQueryBuilder()
-      .insert()
-      .into(Logs)
-      .values({
-        crunchyrollID: items[i].id,
-        createdAt: moment().toDate()
-      })
-      .execute()
+async function* sendWebhook(discordMessages: Discord[]) {
+  for(const message of discordMessages) {
+    yield axios.post(process.env.DISCORD_WEBHOOK || '', message);
   } 
 }
 
@@ -53,17 +27,57 @@ export async function getRSSItemsCrunchyroll() {
   const resp = await parse('http://feeds.feedburner.com/crunchyroll/rss/anime?lang=esLA');
   const { items } = resp as CrunchyrollRSS;
 
+  const ids = items.reduce<string[]>((acc, { id }) => [...acc, id], []);
+
   const logs = db.getRepository<Logs>('Logs');
-  const logsDB = await logs.find({ take: 100 });
-  const logsDBf = (logsDB || []).map(({ crunchyrollID }) => crunchyrollID);
+  const logsDB = await logs.find({ where: { crunchyrollID: In(ids) }});
+  const logsIDs = (logsDB || []).map(({ crunchyrollID }) => crunchyrollID);
 
   const itemsf: Item[] = items
-    .reduce<Item[]>((acc, item, i) => (i < 30 ? [...acc, item] : acc), [])
-    .filter(({ id }) => !logsDBf.includes(id))
-    .reduce<Item[]>((acc, item, i) => (i < 10 ? [...acc, item] : acc), [])
+    .filter(({ id }) => !logsIDs.includes(id))
     .reverse();
 
-  for await (const r of sendWebhook(itemsf)) {}
+  let index = 0;
+  let count = 1;
+  const discordMessages: Discord[] = [];
+  const newLogs: Logs[] = []
+  itemsf.forEach((item) => {
+    if (count === 11) {
+      index++;
+      count = 1;
+    }
+
+    const embed: Embed = {
+      title: item.title,
+      url: item.link,
+      color: 16020769, // #f47521
+      description: moment.unix(item.published/1000).format('LLLL'),
+      image: { url: item.description.replace(/(.+src=\")(.+)(\".+)/, "$2") } ,
+    }
+
+    if (count === 1) {
+      discordMessages.push({ embeds: [embed] });
+    } else {
+      discordMessages[index].embeds.push(embed);
+    }
+
+    const log = new Logs();
+    log.crunchyrollID = item.id;
+    log.createdAt = moment().toDate();
+    newLogs.push(log);
+
+    count++;
+  });
+
+  for await (const r of sendWebhook(discordMessages)) {
+    console.log(r.data);
+  }
+
+  await db.createQueryBuilder()
+    .insert()
+    .into(Logs)
+    .values(newLogs)
+    .execute()
 
   return items;
 }
